6aab86a4426f2c9647ffb400c9087130b2044393
zegheim <justin@aristakas.in>
2021-04-13 01:36:47 +0100

    add tech stack to bottom of page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import dayjs from "dayjs";
 import MqttConnector from "../src/components/connector";
 import Dashboard from "../src/components/dashboard";
+import TechStack from "../src/components/tech-stack";
 import prisma from "../src/lib/prisma";
 
 const DashboardPage = ({ moisture, saturation }) => {
   return (
-    <section className="text-primary text-justify leading-snug px-4">
-      <h2>My Dashboard</h2>
-      <MqttConnector>
-        <Dashboard historical={{ moisture, saturation }} />
-      </MqttConnector>
-    </section>
+    <>
+      <section className="text-primary text-justify leading-snug px-4">
+        <h1>My Dashboard</h1>
+        <MqttConnector>
+          <Dashboard historical={{ moisture, saturation }} />
+        </MqttConnector>
+      </section>
+      <section className="text-primary absolute bottom-0 left-1/2 center-absolute-x">
+        <TechStack />
+      </section>
+    </>
   );
 };
 
---
+++ src/components/tech-stack.js
@@ -0,0 +1,10 @@
+const TechStack = () => (
+  <p className="text-center">
+    Made using <a href="https://nextjs.org">Next.js</a>,{" "}
+    <a href="https://mosquitto.org/">Mosquitto</a>,{" "}
+    <a href="https://www.timescale.com/">Timescale</a>, and{" "}
+    <a href="https://shop.pimoroni.com/products/grow">Pimoroni</a>.
+  </p>
+);
+
+export default TechStack;
