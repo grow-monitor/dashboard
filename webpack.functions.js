2216a0830ae8450e17c21a86e6c31d9dcdab0611
zegheim <justin@aristakas.in>
2021-05-06 18:39:26 +0100

    use the correct setting

--- webpack.functions.js
+++
@@ -1,6 +0,0 @@
-//https://github.com/netlify/netlify-lambda/issues/179#issuecomment-531468792webpack.functions.js
-const nodeExternals = require("webpack-node-externals");
-
-module.exports = {
-  externals: [nodeExternals()],
-};
