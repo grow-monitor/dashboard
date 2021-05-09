6205deec15d2984639f8b7af46aeb09ad150dde9
Justin Arista Kasin <justin+github@aristakas.in>
2021-05-09 22:02:27 +0100

    update tech stack

--- src/components/tech-stack.js
+++ src/components/tech-stack.js
@@ -2,8 +2,9 @@ const TechStack = () => (
   <p className="text-center">
     Made using <a href="https://nextjs.org">Next.js</a>,{" "}
     <a href="https://mosquitto.org/">Mosquitto</a>,{" "}
-    <a href="https://www.timescale.com/">Timescale</a>, and{" "}
-    <a href="https://shop.pimoroni.com/products/grow">Pimoroni</a>.
+    <a href="https://fauna.com/">Fauna</a>, and{" "}
+    <a href="https://shop.pimoroni.com/products/grow">Pimoroni</a>. Hosted on{" "}
+    <a href="https://www.netlify.com/">Netlify</a>.
   </p>
 );
 
